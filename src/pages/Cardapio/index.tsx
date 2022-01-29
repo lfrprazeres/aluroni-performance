7b9c990fe8324178980ad2ec2e7eb70b1b3b0ba3
cicd-hotmart <lfrprazeres@gmail.com>
2022-01-29 19:25:12 -0300

    2.2 - Importando a logo

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,11 @@
 import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 export default function Cardapio() {
   return (
     <main>
       <nav className={styles.menu}>
-        <img src="../../assets/logo.svg" alt="logo do aluroni" />
+        <Logo />
       </nav>
     </main>
   )
