8431da0849e891f06435941248d5c3d7ef37c4c5
Luiz Fernando <lfrprazeres@gmail.com>
2022-02-25 10:27:18 -0300

    3.2 - React developer tools

--- src/routes.tsx
+++ src/routes.tsx
@@ -9,7 +9,6 @@ import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
-  console.log(Cardapio());
   return (
     <main className='container'>
       <Router>
